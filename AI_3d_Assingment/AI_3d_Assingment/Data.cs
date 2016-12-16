b3a6b7448eeee39afd2c2ec7414a7879ab1d9304
Archontellis Rafael Sotirchellis <archontellis29@gmail.com>
2016-12-16 20:55:44 +0200

    16/12 implementation

--- AI_3d_Assingment/AI_3d_Assingment/Data.cs
+++ AI_3d_Assingment/AI_3d_Assingment/Data.cs
@@ -21,19 +21,19 @@ namespace AI_3d_Assingment
             Attrs = new List<String>();
         }
 
-        //print
-        public void printHeader()
+        //print the data 
+        public void print()
         {
-            foreach (String temp in Attrs)
+            foreach (String attr in Attrs)
             {
-                Console.WriteLine("\t" + temp);
+                Console.Write("\t" + attr);
             }
             foreach (int[] temp in rows)
             {
                 Console.WriteLine("");
                 for (int i = 0; i < cols; i++)
                 {
-                    Console.WriteLine("\t" + temp[i]);
+                    Console.Write("\t" + temp[i]);
                 }
             }
             Console.WriteLine("");
@@ -52,65 +52,116 @@ namespace AI_3d_Assingment
         //reads the file an prepare the our matrix data 
         public void prepare(String @filePath, int percentage)
         {
-            string[] lines = System.IO.File.ReadAllLines(filePath);
-            //////////////////////////////headers reader////////////////////////////////
-            {
-                string[] headers = lines[0].Split(' ');
-                cols = headers.Length;
-                foreach (string header in headers)
-                {
-                    Attrs.Add(header);
-                }
-            }
-            ////////////////////////////////data reader/////////////////////////////////
-            {
-                int[] tmpRow = new int[cols];
+            Console.WriteLine(System.IO.File.Exists(filePath) ? "File exists." : "File does not exist.");
 
-                for (int i = 1; i < lines.Length; i++)
+            if (System.IO.File.Exists(filePath))
+            {
+                string[] lines = System.IO.File.ReadAllLines(filePath);
+                //////////////////////////////headers reader////////////////////////////////
                 {
-                    string tmpLine = lines[i];
-                    string[] tmpElements = tmpLine.Split(' ');
-
-                    for (int j = 0; j < tmpElements.Length; j++)
+                    string[] headers = lines[0].Split('\t');
+                    cols = headers.Length + 1;
+                    foreach (string header in headers)
                     {
-                        try
-                        {
-                            tmpRow[j] = Int32.Parse(tmpElements[j]);
-                        }
-                        catch (FormatException e)
-                        {
-                            Console.WriteLine(e.Message, " Cant parse the string");
-                        }
-
-                    }
-
-                    rows.Add(tmpRow);
-                }
-            }
-            /////////////////////////////calculate percentage/////////////////////////////
-            {
-                int finalRowsNum = (percentage * rows.Count) / 100;
-
-                if (!(finalRowsNum == rows.Count))
-                {
-                    for (int i = rows.Count - 1; i > finalRowsNum; i--)
-                    {
-                        rows.RemoveAt(i);
+                        Attrs.Add(header);
                     }
                 }
+
+                Attrs.Add("Result");
+                ////////////////////////////////data reader/////////////////////////////////
+                {
+
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        int[] tmpRow = new int[cols];
+                        string tmpLine = lines[i];
+                        string[] tmpElements = tmpLine.Split('\t');
+
+                        for (int j = 0; j < tmpElements.Length ; j++)
+                        {
+                            try
+                            {
+                                tmpRow[j] = Int32.Parse(tmpElements[j]);
+                            }
+                            catch (FormatException e)
+                            {
+                                Console.WriteLine(e.Message, " Cant parse the string");
+                            }
+
+
+                        }
+
+                        rows.Add(tmpRow);
+                    }
+                }
+                /////////////////////////////calculate percentage/////////////////////////////
+                {
+                    int finalRowsNum = (percentage * rows.Count) / 100;
+
+                    if (!(finalRowsNum == rows.Count))
+                    {
+                        for (int i = rows.Count - 1; i > finalRowsNum; i--)
+                        {
+                            rows.RemoveAt(i);
+                        }
+                    }
+                }
+
+                rowsNum = rows.Count;
+
             }
-
-            rowsNum = rows.Count; 
-            //////////////////////////////////////////////////////////////////////////////
-
-
-
 
 
 
         }
 
+        //with this function i remove the col with a spesific attr name
+        public Data split(string attrName, int value)
+        {
+            Data dataReturn = new Data();
+            List<int[]> rowsReturn = new List<int[]>();
+            List<string> headersReturn = new List<string>();
+            int attrIndex = 0;
 
+            ////////////////////All headers except the selected///////////////////////
+
+            foreach (string tmpHeader in Attrs)
+            {
+                if (!tmpHeader.Equals(attrName))
+                    headersReturn.Add(tmpHeader);
+            }
+
+            dataReturn.cols = headersReturn.Count;
+            dataReturn.Attrs = headersReturn;
+
+            for (attrIndex = 0; attrIndex < Attrs.Count; attrIndex++)
+            {
+                if (attrName.Equals(Attrs[attrIndex]))
+                    break;
+            }
+
+            foreach (int[] tmpRow in rows)
+            {
+                if (tmpRow[attrIndex] == value)
+                {
+                    int[] tmpCol = new int[headersReturn.Count];
+                    int indexTmpCol = 0;
+                    for (int i = 0; i < cols; i++)
+                    {
+                        if (!(i == attrIndex))
+                        {
+                            tmpCol[indexTmpCol] = tmpRow[i];
+                            indexTmpCol++;
+                        }
+                    }
+                    rowsReturn.Add(tmpRow);
+                }
+            }
+
+            dataReturn.rows = rowsReturn;
+            dataReturn.rowsNum = rowsReturn.Count;
+            return dataReturn;
+        }
 
 
     }
